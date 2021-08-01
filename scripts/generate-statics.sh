b50a3ac35a99e7296a4d694ce6ae168004a79340
Bartosz Kupidura <bkupidura@juniper.net>
2021-08-01 23:37:34 +0200

    Use k8s-at-home chart for frigate

--- scripts/generate-statics.sh
+++ scripts/generate-statics.sh
@@ -4,9 +4,5 @@ STATIC_DIR=./static_generated
 mkdir ${STATIC_DIR}
 cd ${STATIC_DIR}
 
-git clone https://github.com/blakeblackshear/billimek-charts.git || pushd billimek-charts && git pull && popd
-helm package --version=0.0 billimek-charts/charts/frigate/
-
 kubectl delete configmap static-generated -n home-infra
-kubectl create configmap static-generated -n home-infra \
-    --from-file=frigate-0.0.tgz=frigate-0.0.tgz
+kubectl create configmap static-generated -n home-infra
